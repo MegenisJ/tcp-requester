d40f6ef5889d44dcb56ca1222a42f932c1a7e748
MegenisJ <j.a.megenis@gmail.com>
2023-11-26 23:33:24 +0000

    move to one function

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
 use std::net::TcpStream;
 
 fn main() {
-    make_request();
-}
-
-
-fn make_request(){
     if let Ok(_stream) = TcpStream::connect("127.0.0.1:7878") {
         println!("Connected!");
     } else{
         println!("failed");
     }    
 }
+
+
 
