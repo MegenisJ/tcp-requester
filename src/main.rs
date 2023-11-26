084f4811e06b0703b9f38188e5170d6ba81b7fc5
MegenisJ <j.a.megenis@gmail.com>
2023-11-26 23:31:34 +0000

    bit o cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,15 @@
-use http::{Request,Response};
 use std::net::TcpStream;
 
 fn main() {
     make_request();
-    println!("Hello, world!");
 }
 
 
 fn make_request(){
-    let request = Request::builder()
-        .uri("https://www.rust-lang.org/")
-        .header("User-Agent", "jimbo-jambo-agent/1.0");
-     send(request.body(()).unwrap());
-    
-}
-
-fn send(_req: Request<()>) {
     if let Ok(_stream) = TcpStream::connect("127.0.0.1:7878") {
         println!("Connected!");
     } else{
         println!("failed");
-    }
-
+    }    
 }
+
