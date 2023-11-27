4b63dddb2cb3ec9c10df71e5e3913db52aa58936
MegenisJ <j.a.megenis@gmail.com>
2023-11-27 14:59:42 +0000

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use std::net::TcpStream;
+use std::{
+    io::{Read,Write},
+    net::TcpStream
+};
+fn main() -> std::io::Result<()>{
+    let mut stream = TcpStream::connect("127.0.0.1:7878").unwrap();
+    stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+    let mut line = String::new();
+    let _result = stream.read_to_string(&mut line);
+    println!("output {}", line);
 
-fn main() {
-    if let Ok(_stream) = TcpStream::connect("127.0.0.1:7878") {
-        println!("Connected!");
-    } else{
-        println!("failed");
-    }    
+    Ok(())
 }
-
-
 
